a01bec57025da62aba689adb9543a881808d1d3c
Davin <davin-dj@hotmail.com>
2022-10-13 12:17:17 -0400

    Created classes.js and utility.js along with the noteLibrary object

---
+++ js/utility.js
@@ -0,0 +1,4 @@
+const notesLibrary = {
+    sharpFormat: ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'],
+    flatFormat: ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
+}
