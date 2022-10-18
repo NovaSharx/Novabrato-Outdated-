50e8e51b0097579814d804e2c14bc94e20cad97e
Davin <davin-dj@hotmail.com>
2022-10-18 15:16:41 -0400

    Seperated noteFormats from noteLibrary

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const noteLibrary = ['A', 'A#/Bb', 'B', 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab']
+
+const noteFormats = {
+    flatFormat: ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab'],
+    sharpFormat: ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+}
+
+const firstString = document.getElementById('first-string')
+const secondString = document.getElementById('second-string')
+const thirdString = document.getElementById('third-string')
+const forthString = document.getElementById('forth-string')
+const fifthString = document.getElementById('fifth-string')
+const sixthString = document.getElementById('sixth-string')
+
+function tuneString(string, note) {
+    
+}
--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,2 @@
-const notesLibrary = {
-    sharpFormat: ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'],
-    flatFormat: ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
-}
+
+
